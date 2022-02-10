7bbdd064e367b94c010f825536015301201b970a
tellgreenpo <mateo1000a@gmail.com>
2022-02-10 18:16:16 +0100

    changed condition button pressed ring mill

--- src/base_functions.cpp
+++ src/base_functions.cpp
@@ -4,43 +4,39 @@ namespace base_functions
 {
 
 	// Global variables
-	bool ring_mill_already_pressed = false;
+	bool button_last_state = false;
 	bool pneumatic_already_pressed = false;
 
 	// FIXME LOOPS IN TOO QUICK SO BUGGED CHANGE
 	void activate_ring_mill(std::shared_ptr<Motor> ringMillMotor, bool button_pressed)
 	{
-		if (button_pressed)
+		if (button_pressed && (button_pressed != button_last_state))
 		{
-			if (ring_mill_already_pressed)
-			{
-				ringMillMotor->moveVelocity(0);
-				ring_mill_already_pressed = false;
-			}
-			else
-			{
-				ringMillMotor->moveVelocity(200);
-				ring_mill_already_pressed = true;
-			}
+			ringMillMotor->moveVelocity(200);
+			button_last_state = button_pressed;
+		}
+		if (!button_pressed && (button_pressed != button_last_state))
+		{
+			ringMillMotor->moveVelocity(0);
+			button_last_state = button_pressed;
 		}
 	}
 
-	// FIXME LOOPS IN TOO QUICK SO BUGGED CHANGE
-	void activate_pneumatic(std::shared_ptr<pros::ADIPort> pneumaticPort, bool button_pressed)
+// FIXME LOOPS IN TOO QUICK SO BUGGED CHANGE
+void activate_pneumatic(std::shared_ptr<pros::ADIPort> pneumaticPort, bool button_pressed)
+{
+	if (button_pressed)
 	{
-		if (button_pressed)
+		if (pneumatic_already_pressed)
 		{
-			if (pneumatic_already_pressed)
-			{
-				pneumaticPort->set_value(LOW);
-				pneumatic_already_pressed = false;
-			}
-			else
-			{
-				pneumaticPort->set_value(HIGH);
-				pneumatic_already_pressed = true;
-			}
+			pneumaticPort->set_value(LOW);
+			pneumatic_already_pressed = false;
+		}
+		else
+		{
+			pneumaticPort->set_value(HIGH);
+			pneumatic_already_pressed = true;
 		}
 	}
-
+}
 }
