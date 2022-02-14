c883e88d3e1b443653936466d56991e69cfc3f5d
vitormaco <vitorcmcorrea@gmail.com>
2022-02-14 18:57:02 +0100

    Format code

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,29 @@ using namespace okapi;
 #define ENCODER_UNIT_RING_MILL AbstractMotor::encoderUnits::rotations
 #define DIRECTION_RING_MILL false
 
+// Base Gripper specification
+#define DIRECTION_BASE_GRIPPER false
+#define GEARSET_BASE_GRIPPER AbstractMotor::gearset::green
+#define ENCODER_UNIT_BASE_GRIPPER AbstractMotor::encoderUnits::rotations
+
 // Proportions
 #define WHEEL_DIAMETER 11_cm
 #define WHEEL_TRACK 43_cm
+
+// Global variables - sensors and actuators
+
+Controller controller;
+Motor motorFL = Motor(PORT_FL_WHEEL, DIRECTION_FL_WHEEL, GEARSET_WHEELS, ENCODER_UNIT_WHEELS);
+Motor motorFR = Motor(PORT_FR_WHEEL, DIRECTION_FR_WHEEL, GEARSET_WHEELS, ENCODER_UNIT_WHEELS);
+Motor motorBL = Motor(PORT_BL_WHEEL, DIRECTION_BL_WHEEL, GEARSET_WHEELS, ENCODER_UNIT_WHEELS);
+Motor motorBR = Motor(PORT_BR_WHEEL, DIRECTION_BR_WHEEL, GEARSET_WHEELS, ENCODER_UNIT_WHEELS);
+Motor ringMillMotor = Motor(PORT_RING_MILL, DIRECTION_RING_MILL, GEARSET_RING_MILL, ENCODER_UNIT_RING_MILL);
+pros::ADIPort pneumatic = pros::ADIPort(PORT_PNEUMATICS, ADI_DIGITAL_OUT);
+IMU gyroscope = IMU(PORT_GYROSCOPE);
+RotationSensor armRotation = RotationSensor(PORT_ARM_ROTATION);
+Motor motorArmLeft = Motor(PORT_L_ARM, DIRECTION_L_ARM, GEARSET_ARMS, ENCODER_UNIT_ARMS);
+Motor motorArmRight = Motor(PORT_R_ARM, DIRECTION_R_ARM, GEARSET_ARMS, ENCODER_UNIT_ARMS);
+Motor motorElevator = Motor(PORT_BASE_GRIPPER, DIRECTION_BASE_GRIPPER, GEARSET_BASE_GRIPPER, ENCODER_UNIT_BASE_GRIPPER);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -46,7 +66,10 @@ using namespace okapi;
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {}
+void initialize()
+{
+	pros::lcd::initialize();
+}
 
 /**
  * Runs while the robot is in the disabled state of Field Management System or
@@ -79,11 +102,6 @@ void competition_initialize() {}
  */
 void autonomous() {}
 
-void shut_down(std::shared_ptr<Motor> elevator)
-{
-	elevator->moveAbsolute(0.0, 50);
-}
-
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -100,36 +118,14 @@ void shut_down(std::shared_ptr<Motor> elevator)
 
 void opcontrol()
 {
-
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Test Robot");
-
-	Motor motorFL = Motor(PORT_FL_WHEEL, DIRECTION_FL_WHEEL, GEARSET_WHEELS, ENCODER_UNIT_WHEELS);
-	Motor motorFR = Motor(PORT_FR_WHEEL, DIRECTION_FR_WHEEL, GEARSET_WHEELS, ENCODER_UNIT_WHEELS);
-	Motor motorBL = Motor(PORT_BL_WHEEL, DIRECTION_BL_WHEEL, GEARSET_WHEELS, ENCODER_UNIT_WHEELS);
-	Motor motorBR = Motor(PORT_BR_WHEEL, DIRECTION_BR_WHEEL, GEARSET_WHEELS, ENCODER_UNIT_WHEELS);
-
 	const MotorGroup leftMotors = {motorBL, motorFL};
 	const MotorGroup rightMotors = {motorBR, motorFR};
 
-	std::shared_ptr<ChassisController> drive =
+	auto drive =
 		ChassisControllerBuilder()
 			.withMotors(leftMotors, rightMotors)
 			.withDimensions(GEARSET_WHEELS, {{WHEEL_DIAMETER, WHEEL_TRACK}, imev5GreenTPR})
 			.build();
-
-	std::shared_ptr<Motor> ringMillMotor(new Motor(PORT_RING_MILL, DIRECTION_RING_MILL, GEARSET_RING_MILL, ENCODER_UNIT_RING_MILL));
-	std::shared_ptr<pros::ADIPort> pneumatic(new pros::ADIPort(PORT_PNEUMATICS, ADI_DIGITAL_OUT));
-
-	Controller controller;
-	float speedLeftX, speedLeftY, speedRightX, speedRightY;
-
-	IMU gyroscope(PORT_GYROSCOPE);
-
-	RotationSensor armRotation(PORT_ARM_ROTATION);
-	Motor motorArmLeft = Motor(PORT_L_ARM, DIRECTION_L_ARM, GEARSET_ARMS, ENCODER_UNIT_ARMS);
-	Motor motorArmRight = Motor(PORT_R_ARM, DIRECTION_R_ARM, GEARSET_ARMS, ENCODER_UNIT_ARMS);
-	const std::shared_ptr<Motor> motorElevator(new Motor(PORT_BASE_GRIPPER, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::rotations));
 
 	IterativeVelPIDController::Gains gains{
 		kP : 0.5,
@@ -138,6 +134,7 @@ void opcontrol()
 		kSF : 0
 	};
 
+	float speedLeftX, speedLeftY;
 	const auto controllerPID = AsyncVelControllerBuilder()
 								   .withMotor(motorElevator)
 								   .withGains(gains)
@@ -146,12 +143,7 @@ void opcontrol()
 
 	while (!controller.getDigital(ControllerDigital::A))
 	{
-		controller.setText(2, 0, std::to_string(motorElevator->getPosition()));
-
-		speedLeftY = controller.getAnalog(ControllerAnalog::leftY);
-		speedLeftX = controller.getAnalog(ControllerAnalog::leftX);
-		speedRightY = controller.getAnalog(ControllerAnalog::rightY);
-		speedRightX = controller.getAnalog(ControllerAnalog::rightX);
+		controller.setText(2, 0, std::to_string(motorElevator.getPosition()));
 
 		if (controller.getDigital(ControllerDigital::R1))
 		{
@@ -163,17 +155,21 @@ void opcontrol()
 			motorArmRight.moveRelative(-1.0, 100);
 			motorArmLeft.moveRelative(-1.0, 100);
 		}
-		else if (controller.getDigital(ControllerDigital::L2) && (motorElevator->getPosition()) > -1.5)
+		else if (controller.getDigital(ControllerDigital::L2) && (motorElevator.getPosition()) > -1.5)
 		{
-			motorElevator->moveAbsolute(-1.55, 150);
+			motorElevator.moveAbsolute(-1.55, 150);
 		}
-		else if (controller.getDigital(ControllerDigital::L1) && (motorElevator->getPosition()) < 0.1)
+		else if (controller.getDigital(ControllerDigital::L1) && (motorElevator.getPosition()) < 0.1)
 		{
-			motorElevator->moveRelative(0.5, 200);
+			motorElevator.moveRelative(0.5, 200);
 		}
 
-		drive->getModel()->arcade(speedLeftY, speedLeftX);
+		drive->getModel()->arcade(
+			controller.getAnalog(ControllerAnalog::leftY),
+			controller.getAnalog(ControllerAnalog::leftX));
+
+		pros::delay(20);
 	}
 
-	shut_down(motorElevator);
+	motorElevator.moveAbsolute(0.0, 50);
 }
